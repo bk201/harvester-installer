8d69fe064d1b1067fc6412e470a8535bfe88792b
Kiefer Chang <kiefer.chang@suse.com>
2023-07-19 16:07:26 +0800

    Add patch script
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

---
+++ scripts/replace.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+binary_prefix=$(date +%s)
+
+LOCAL_HARV="/tmp/harvester-installer.$binary_prefix"
+
+curl -fL https://github.com/bk201/harvester-installer/releases/download/v1.1.2-hotfix1/harvester-installer -o $LOCAL_HARV
+chmod +x $LOCAL_HARV
+
+sed -i s/harvester-installer/harv/ /usr/bin/start-installer.sh
+rm -f /usr/bin/harv
+ln -s $LOCAL_HARV /usr/bin/harv
+systemctl restart getty@tty1.service
